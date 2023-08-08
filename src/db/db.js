23f80d3be3aab734e707df1f51b220d3bfbd1ebd
jpvergaralb <jpvergaralb@uc.cl>
2023-08-08 14:27:50 -0400

    add port to db connection

--- src/db/db.js
+++ src/db/db.js
@@ -6,5 +6,13 @@ export const sequelize = new Sequelize(
   process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
   {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
   dialect: 'postgres'
 })
+
+try {
+  await sequelize.authenticate()
+  console.log('Connection has been established successfully.')
+} catch (error) {
+  console.error('Unable to connect to the database:', error)
+}
