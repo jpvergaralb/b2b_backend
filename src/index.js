f41b33c4f85e84a143b15a1ecdf36978100582c0
jpvergaralb <jpvergaralb@uc.cl>
2023-08-09 22:55:27 -0400

    (main server) add: db connection

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import { taskRoutes } from './routes/task.routes.js'
 import { requestLoggerMiddleware } from './middlewares/requestLogger.middleware.js'
 import dotenv from 'dotenv'
 dotenv.config()
+import { sequelize } from './db/db.js'
+import { User, Subject, Task } from './models/models.js'
+
           
-
-
 const app = express()
+
+// db
+const syncDatabase = async () => {
+  try {
+    await sequelize.sync({alter: true})
+    console.log('Connection has been established successfully from main server.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+syncDatabase()
 
 // Middlewares
 app.use(requestLoggerMiddleware)
@@ -19,6 +31,7 @@ app.use("/subjects", subjectRouter)
 app.use("/tasks", taskRoutes)
 
 
+// Server
 const port = process.env.PORT || 8000
 app.listen(port, () => {
   console.log(`Running server on ${port}...`)
