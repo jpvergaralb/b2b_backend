00b6a8b8286d992354f4cdd415435d9f6c569cb6
jpvergaralb <jpvergaralb@uc.cl>
2023-08-09 22:54:10 -0400

    (db) update: db env variable

--- src/db/db.js
+++ src/db/db.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
+process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, 
   {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
+  port: process.env.DB_PORT || 5434,
   dialect: 'postgres'
 })
+  
+
+
+
 
 try {
   await sequelize.authenticate()
