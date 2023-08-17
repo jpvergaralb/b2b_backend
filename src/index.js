4400d8bf50a8d40c68f51b57f17dc25fe86eb054
jpvergaralb <jpvergaralb@uc.cl>
2023-08-17 11:14:29 -0400

    delete all db configs and things: migrating to migrations

--- src/index.js
+++ src/index.js
@@ -5,22 +5,10 @@ import { taskRoutes } from './routes/task.routes.js'
 import { requestLoggerMiddleware } from './middlewares/requestLogger.middleware.js'
 import dotenv from 'dotenv'
 dotenv.config()
-import { sequelize } from './db/db.js'
-import { User, Subject, Task } from './models/models.js'
-
           
 const app = express()
 
-// db
-const syncDatabase = async () => {
-  try {
-    await sequelize.sync({alter: true})
-    console.log('Connection has been established successfully from main server.')
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
-syncDatabase()
+
 
 // Middlewares
 app.use(requestLoggerMiddleware)
