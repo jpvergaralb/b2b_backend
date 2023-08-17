4266fdc1f0d7c0a24a18d7b3fbf8a154402430a5
jpvergaralb <jpvergaralb@uc.cl>
2023-08-17 14:30:28 -0400

    add current migrations

---
+++ migrations/20230817181026-add-password-to-users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('users', 'password', 
+      {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          min: 6,
+          isStrong(value) {
+            if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/.test(value)) {
+          throw new Error('La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.');
+            }
+          }
+        }
+      }
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
