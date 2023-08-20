8881e0514def87384f4b38021298140577fe46a7
jpvergaralb <jpvergaralb@uc.cl>
2023-08-20 11:09:43 -0400

    change to commonjs

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
-import express from 'express'
-import { userRoutes }  from './routes/user.routes.js'
-import { subjectRouter } from './routes/subject.routes.js'
-import { taskRoutes } from './routes/task.routes.js'
-import { requestLoggerMiddleware } from './middlewares/requestLogger.middleware.js'
-import dotenv from 'dotenv'
-dotenv.config()
-          
-const app = express()
+const express = require('express');
+const userRoutes = require('./routes/user.routes');
+const courseRouter = require('./routes/course.routes');
+const taskRoutes = require('./routes/task.routes');
+const requestLoggerMiddleware = require('./middlewares/requestLogger.middleware');
+require('dotenv').config();
 
-
+const app = express();
 
 // Middlewares
-app.use(requestLoggerMiddleware)
+app.use(requestLoggerMiddleware);
 
 // Routes
-app.use("/users", userRoutes)
-app.use("/subjects", subjectRouter)
-app.use("/tasks", taskRoutes)
-
+app.use('/users', userRoutes);
+app.use('/courses', courseRouter);
+app.use('/tasks', taskRoutes);
 
 // Server
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Running server on ${port}...`)
-})
-
+  // eslint-disable-next-line no-console
+  console.log(`Running server on ${port}...`);
+});
