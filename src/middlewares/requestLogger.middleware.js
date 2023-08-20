0ccb77ab0f1aca3c97512773952852afb2fd6fdf
jpvergaralb <jpvergaralb@uc.cl>
2023-08-20 11:09:28 -0400

    add eslint restriction

--- src/middlewares/requestLogger.middleware.js
+++ src/middlewares/requestLogger.middleware.js
@@ -1,4 +1,7 @@
-export const requestLoggerMiddleware = async (req, res, next) => {
-  console.log(`Received a ${req.method} request for ${req.url}.`)
-  next()
-}
+const requestLoggerMiddleware = async (req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.log(`Received a ${req.method} request for ${req.url}.`);
+  next();
+};
+
+module.exports = requestLoggerMiddleware;
