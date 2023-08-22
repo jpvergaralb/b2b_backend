3fa9098b459ca9a24c8acd26b4dcc13c9ab7667f
jpvergaralb <jpvergaralb@uc.cl>
2023-08-22 10:47:57 -0400

    add testing env config
    add env variables for testing db

--- config/config.cjs
+++ config/config.cjs
@@ -9,5 +9,13 @@ module.exports = {
         host: process.env.DB_HOST,
         dialect: 'postgres',
         logging: console.log,
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.TEST_DB_NAME,
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
     }
 }
