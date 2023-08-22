c76d203a87fc045d4a6a1c1b2d999fe546e2bb18
jpvergaralb <jpvergaralb@uc.cl>
2023-08-22 10:48:19 -0400

    add welcome route
    just a get for testing purposes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const welcomeRoutes = require('./routes/welcome.routes');
 const userRoutes = require('./routes/user.routes');
 const courseRouter = require('./routes/course.routes');
 const taskRoutes = require('./routes/task.routes');
@@ -12,6 +13,7 @@ app.use(requestLoggerMiddleware);
 app.use(express.json());
 
 // Routes
+app.use('/', welcomeRoutes)
 app.use('/users', userRoutes);
 app.use('/courses', courseRouter);
 app.use('/tasks', taskRoutes);
