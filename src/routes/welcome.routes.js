3863b5cf1dfa4549210d15e48f985fd7f4a55e18
jpvergaralb <jpvergaralb@uc.cl>
2023-08-22 10:49:00 -0400

    simple welcome route for testing purposes

---
+++ src/routes/welcome.routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const {
+    getWelcome
+}
+= require('../controllers/welcome.controller')
+
+const welcomeRouter = express.Router();
+
+
+welcomeRouter.route('/')
+    .get(getWelcome)
+
+module.exports = welcomeRouter
