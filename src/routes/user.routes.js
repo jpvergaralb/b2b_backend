d4099d316deb9050837e34757fa0a2306cc1c3e6
jpvergaralb <jpvergaralb@uc.cl>
2023-08-27 23:08:20 -0400

    add login/register/me routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,20 +2,30 @@ const express = require('express');
 const {
   getUser,
   getUsers,
+  loginUser,
   postUser,
   updateUser,
   deleteUser,
   inscribeUserToCourse,
 } = require('../controllers/user.controllers');
 
+const validateToken = require('../middlewares/validateAuthToken.middleware');
+
 const userRoutes = express.Router();
 
 userRoutes.route('/')
   .get(getUsers)
+  
+userRoutes.route('/signup')
   .post(postUser);
 
+userRoutes.route('/me')
+  .get(validateToken, getUser);
+
+userRoutes.route('/login')
+  .post(loginUser);
+
 userRoutes.route('/:id')
-  .get(getUser)
   .put(updateUser)
   .delete(deleteUser)
   .post(inscribeUserToCourse);
